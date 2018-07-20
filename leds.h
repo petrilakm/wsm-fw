1e0bec3d68c258687822023c14fab5f05b9855ea
Jan Horacek <me@apophis.cz>
2018-07-20 16:18:57 +0200

    New LEDs pinout.

--- leds.h
+++ leds.h
@@ -4,20 +4,20 @@
 #include <avr/io.h>
 
 void leds_init() {
-	DDRC |= 0x70;
-	PORTC |= 0x70;
+	DDRC |= 0x0E;
+	PORTC |= 0x0E;
 }
 
-void led_red_on() { PORTC &= ~(1 << PORTC4); }
-void led_red_off() { PORTC |= 1 << PORTC4; }
-void led_red_toggle() { PORTC ^= 1 << PORTC4; }
+void led_red_on() { PORTC &= ~(1 << PORTC3); }
+void led_red_off() { PORTC |= 1 << PORTC3; }
+void led_red_toggle() { PORTC ^= 1 << PORTC3; }
 
-void led_yellow_on() { PORTC &= ~(1 << PORTC5); }
-void led_yellow_off() { PORTC |= 1 << PORTC5; }
-void led_yellow_toggle() { PORTC ^= 1 << PORTC5; }
+void led_yellow_on() { PORTC &= ~(1 << PORTC2); }
+void led_yellow_off() { PORTC |= 1 << PORTC2; }
+void led_yellow_toggle() { PORTC ^= 1 << PORTC2; }
 
-void led_green_on() { PORTC &= ~(1 << PORTC6); }
-void led_green_off() { PORTC |= 1 << PORTC6; }
-void led_green_toggle() { PORTC ^= 1 << PORTC6; }
+void led_green_on() { PORTC &= ~(1 << PORTC1); }
+void led_green_off() { PORTC |= 1 << PORTC1; }
+void led_green_toggle() { PORTC ^= 1 << PORTC1; }
 
 #endif
