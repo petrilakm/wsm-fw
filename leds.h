fcac8790cb8500c0cc3bed6bfa6853e9e00d20fc
Jan Horacek <me@apophis.cz>
2018-08-15 12:57:48 +0200

    Make header functions inline.

--- leds.h
+++ leds.h
@@ -3,21 +3,21 @@
 
 #include <avr/io.h>
 
-void leds_init() {
+static inline void leds_init() {
 	DDRC |= 0x0E;
 	PORTC |= 0x0E;
 }
 
-void led_red_on() { PORTC &= ~(1 << PORTC3); }
-void led_red_off() { PORTC |= 1 << PORTC3; }
-void led_red_toggle() { PORTC ^= 1 << PORTC3; }
+static inline void led_red_on() { PORTC &= ~(1 << PORTC3); }
+static inline void led_red_off() { PORTC |= 1 << PORTC3; }
+static inline void led_red_toggle() { PORTC ^= 1 << PORTC3; }
 
-void led_yellow_on() { PORTC &= ~(1 << PORTC2); }
-void led_yellow_off() { PORTC |= 1 << PORTC2; }
-void led_yellow_toggle() { PORTC ^= 1 << PORTC2; }
+static inline void led_yellow_on() { PORTC &= ~(1 << PORTC2); }
+static inline void led_yellow_off() { PORTC |= 1 << PORTC2; }
+static inline void led_yellow_toggle() { PORTC ^= 1 << PORTC2; }
 
-void led_green_on() { PORTC &= ~(1 << PORTC1); }
-void led_green_off() { PORTC |= 1 << PORTC1; }
-void led_green_toggle() { PORTC ^= 1 << PORTC1; }
+static inline void led_green_on() { PORTC &= ~(1 << PORTC1); }
+static inline void led_green_off() { PORTC |= 1 << PORTC1; }
+static inline void led_green_toggle() { PORTC ^= 1 << PORTC1; }
 
 #endif
